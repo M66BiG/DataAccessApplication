b82441d222d62d162ed069e922a9e1f9bb49210d
Melih aus Beeck <melihesad@hotmail.de>
2024-01-19 03:41:42 +0100

    Projektdateien hinzufügen.

---
+++ DataAccessLibrary/Data/PeopleData.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLibrary.Models;
+using System.Threading.Tasks.Dataflow;
+
+namespace DataAccessLibrary.Data;
+
+public class PeopleData : IPeopleData
+{
+    private readonly ISqlDataAccess _sql;
+
+    public PeopleData(ISqlDataAccess sql)
+    {
+        _sql = sql;
+    }
+
+    public async Task<IEnumerable<PersonModel>> GetAllPeople()
+    {
+        var output = await _sql.LoadDataAsync<PersonModel, dynamic>(
+            "dbo.spPeople_GetAll",
+            new { } //erstellt ein neuen Objekt
+            // hier hätte ein connectionstring noch hinzugefügt werden können, da es nicht deklariert wird
+            //wird es als "Default" deklariert.
+            );
+        return output;
+    }
+
+    public async Task UpdatePerson(PersonModel person)
+    {
+        await _sql.SaveDataAsync("dbo.spPeople_Update", person);
+    }
+
+    public async Task InsertPerson(PersonModel person)
+    {
+        await _sql.SaveDataAsync("dbo.spPeople_Insert",
+            new { person.FirstName, person.LastName});
+    }
+
+    public async Task DeletePerson(int id)
+    {
+        await _sql.SaveDataAsync("dbo.spPeople_Delete",
+            new { Id = id });
+    }
+}
---
+++ DataAccessLibrary/Data/SqlDataAccess.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Dapper;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+
+namespace DataAccessLibrary.Data;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _config;
+
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task<IEnumerable<T>> LoadDataAsync<T, U>(string storedProcedure, 
+        U parameters, 
+        string connectionStringName = "Default") 
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName); 
+
+        using IDbConnection connection = new SqlConnection(connectionString); 
+     
+
+        var rows = await connection.QueryAsync<T>(storedProcedure, 
+            parameters,
+            commandType: CommandType.StoredProcedure);
+
+        return rows;
+    }
+
+    public async Task SaveDataAsync<T>(string storedProcedure,
+        T parameters,
+        string connectionStringName = "Default")
+    {
+        string connectionString = _config.GetConnectionString(connectionStringName);
+
+        using IDbConnection connection = new SqlConnection(connectionString);
+
+        try
+        {
+            await connection.ExecuteAsync(storedProcedure,
+            parameters,
+            commandType: CommandType.StoredProcedure);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        //chjangesyx
+
+       
+    }
+}
